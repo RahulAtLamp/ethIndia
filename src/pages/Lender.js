e7c8bfad1a0ffb06ea17d31ad37f57560b6fa446
rahulatlamp <rahulrajan@lampros.tech>
2022-12-03 08:26:12 +0530

    Lending page added

--- src/pages/Lender.js
+++ src/pages/Lender.js
@@ -4,12 +4,91 @@ import "../styles/lender.scss";
 function Lender() {
   return (
     <div className="lender-main">
+      <h1 className="lender-header">Lending Requests</h1>
       <div className="lender-inner">
         <div className="lender-card">
           <div className="lender-certificate">
-            0x084c145f98C975a71a2fD5d3E5eAB84c0FC52fDf
+            <img className="lender-certificate-image" src="certificate.png" alt="certificates" />
           </div>
           <div className="lender-address">0x084c145f98C975a71a2fD5d3E5eAB84c0FC52fDf</div>
+          <div className="lender-requests">
+            <div className="lender-confirm">
+              <button className="confirm">Confirm</button>
+            </div>
+            <div className="lender-deny">
+              <button className="deny">Deny</button>
+            </div>
+          </div>
+        </div>
+        <div className="lender-card">
+          <div className="lender-certificate">
+            <img className="lender-certificate-image" src="certificate.png" alt="certificates" />
+          </div>
+          <div className="lender-address">0x084c145f98C975a71a2fD5d3E5eAB84c0FC52fDf</div>
+          <div className="lender-requests">
+            <div className="lender-confirm">
+              <button className="confirm">Confirm</button>
+            </div>
+            <div className="lender-deny">
+              <button className="deny">Deny</button>
+            </div>
+          </div>
+        </div>
+        <div className="lender-card">
+          <div className="lender-certificate">
+            <img className="lender-certificate-image" src="certificate.png" alt="certificates" />
+          </div>
+          <div className="lender-address">0x084c145f98C975a71a2fD5d3E5eAB84c0FC52fDf</div>
+          <div className="lender-requests">
+            <div className="lender-confirm">
+              <button className="confirm">Confirm</button>
+            </div>
+            <div className="lender-deny">
+              <button className="deny">Deny</button>
+            </div>
+          </div>
+        </div>
+        <div className="lender-card">
+          <div className="lender-certificate">
+            <img className="lender-certificate-image" src="certificate.png" alt="certificates" />
+          </div>
+          <div className="lender-address">0x084c145f98C975a71a2fD5d3E5eAB84c0FC52fDf</div>
+          <div className="lender-requests">
+            <div className="lender-confirm">
+              <button className="confirm">Confirm</button>
+            </div>
+            <div className="lender-deny">
+              <button className="deny">Deny</button>
+            </div>
+          </div>
+        </div>
+        <div className="lender-card">
+          <div className="lender-certificate">
+            <img className="lender-certificate-image" src="certificate.png" alt="certificates" />
+          </div>
+          <div className="lender-address">0x084c145f98C975a71a2fD5d3E5eAB84c0FC52fDf</div>
+          <div className="lender-requests">
+            <div className="lender-confirm">
+              <button className="confirm">Confirm</button>
+            </div>
+            <div className="lender-deny">
+              <button className="deny">Deny</button>
+            </div>
+          </div>
+        </div>
+        <div className="lender-card">
+          <div className="lender-certificate">
+            <img className="lender-certificate-image" src="certificate.png" alt="certificates" />
+          </div>
+          <div className="lender-address">0x084c145f98C975a71a2fD5d3E5eAB84c0FC52fDf</div>
+          <div className="lender-requests">
+            <div className="lender-confirm">
+              <button className="confirm">Confirm</button>
+            </div>
+            <div className="lender-deny">
+              <button className="deny">Deny</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
